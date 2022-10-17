8b6cd5c7106049ceb2f695692974b5edf3d195c9
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-10-17 14:34:45 +0530

    Created the electron gui window.

--- window.js
+++ window.js
@@ -1,1 +1,27 @@
-console.log("Namaste World!")
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 16*70,
+    height: 9*70,
+    autoHideMenuBar: true
+  });
+
+  win.loadFile('Web/index.html');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
