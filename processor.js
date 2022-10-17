f101666713349599ef4c46fd268769c1214dedaf
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-10-17 16:32:47 +0530

    Added the card compponent dynamically in the main-grp.

--- processor.js
+++ processor.js
@@ -4,6 +4,12 @@ const fs = require('fs');
 var main_grp = document.getElementById('main-grp')
 fs.readdir(songContainer, (err, files) => {
   files.forEach(file => {
-    main_grp.innerHTML += `${file}`
+    // let song_name = file[:];
+    main_grp.innerHTML += `
+    <div class="card">
+    <div class="song-name">${file.slice(0, -4)}</div>
+    <img src="./music_note.svg">
+    <audio controls src="../Music/${file}"></audio>
+    </div>`
   });
 });
